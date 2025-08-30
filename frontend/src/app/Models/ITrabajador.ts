27c91773cac05d6e5d6ba90115ced6419f13c759
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-08-29 20:48:40 -0600

    trabajador component

--- frontend/src/app/Models/ITrabajador.ts
+++
@@ -1,6 +0,0 @@
-export interface ITrabajador {
-  id: number;
-  nombre: string;
-  proyecto: string;
-  horas: number;
-}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import {TrabajadorComponent} from './Components/trabajador/trabajador.component';
 
 export const routes: Routes = [
 
-
+  {
+    path: '',component: TrabajadorComponent
+  }
 
 ];
